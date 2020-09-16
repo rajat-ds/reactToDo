4c874b848b9d1aa0b38a7bd00ef4f593d1ac236a
rajat-ds <rajatkumarbind@gmail.com>
2020-09-16 21:01:04 +0530

    implemented basic todo app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState} from 'react';
 import './App.css';
 
+
+
 function App() {
+  const defaultList  = [
+    { name: "ItemOne" ,del:"del-1" ,paraId:"para-1" },
+    { name: "ItemTwo",del:"del-2",paraId:"para-2"  },
+    { name: "ItemThree" ,del:"del-3",paraId:"para-3" }
+
+  ]
+  const count = 0 ;
+  const [list, updateList] = useState(defaultList );
+  
+  const handleRemoveItem = idx => {
+    // assigning the list to temp variable
+    const temp = [...list];
+
+    // removing the element using splice
+    temp.splice(idx, 1);
+
+    // updating the list
+    updateList(temp);
+}
+ const addTodo=()=>{
+     
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <header>
+          <h1 className="header">React Todos</h1>
+        </header>
+        <input type="text" id="input" maxLength={80} className="inputText"></input>
+        <button id="add-btn" className="add" onClick={addTodo}>Add</button>
+     
+    {       list.map((item, idx) => {
+                            return(
+                              <div key={idx} className="todoText" >
+                              <button onClick={() =>handleRemoveItem(idx)} id={item.del}> Remove </button>
+                              <p id={item.paraId}>{item.name}</p>
+                              </div>
+                            )  
+            })
+    }
+
+
     </div>
   );
 }
